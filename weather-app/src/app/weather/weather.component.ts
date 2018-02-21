76a6806c5c1282e90ec7ef21eabfff7260739012
Elenazuokaite <elena.zuokaite@gmail.com>
2018-02-21 18:23:43 +0200

    Tabs work

--- weather-app/src/app/weather/weather.component.ts
+++ weather-app/src/app/weather/weather.component.ts
@@ -14,23 +14,28 @@ cityList: any;
   ngOnInit() {
     this.cityList = [];
   }
-  onSearchChange(searchValue) {
-    if (searchValue.length >= 3) {
-      this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + searchValue
-        + '&appid=8b14eb2c5beed13dd28c80dffdf74d42').subscribe(weather => {
-        this.weather = weather;
-        console.log(this.weather);
-          console.log(this.weather.main.temp);
-      });
-    }
+  // onSearchChange(searchValue) {
+  //   if (searchValue.length >= 3) {
+  //     this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + searchValue
+  //       + '&appid=8b14eb2c5beed13dd28c80dffdf74d42').subscribe(weather => {
+  //       this.weather = weather;
+  //       console.log(this.weather);
+  //         console.log(this.weather.main.temp);
+  //     });
+  //   }
 
-  }
+  // }
   onChange(value, apiId) {
-    this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + value + '&appid=' + apiId).subscribe(weather => {
+    this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + value + '&units=metric&appid=' + apiId).subscribe(weather => {
       this.weather = weather;
       console.log(this.cityList);
       this.cityList.push(this.weather);
       console.log('veikia');
+      console.log(document.querySelector('.tab-pane'));
+      // document.querySelectorAll('.nav-link')[this.cityList.length - 1].classList.toggle('active');
+      //   document.querySelectorAll('.nav-link')[0].classList.toggle('show');
+      //   document.querySelectorAll('.tab-pane')[0].classList.toggle('active');
+      //   document.querySelectorAll('.tab-pane')[0].classList.toggle('show');
     });
   }
 
